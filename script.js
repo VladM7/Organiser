93008f57e3ba1512cd446bcc3c220d7f07d5a5c3
VladM7 <mvlad2005@outlook.com>
2021-06-04 00:34:32 +0300

    docs formatting

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var colors = [/*background*/"#FFFFFF", "#353B40", "#5E6366", /*text*/ "#000000"];
+/*
+TODO change semantic ui button colors when in dark mode
+TODO change list appearance
+TODO modify ui structure
+*/
+
+var colors = [
+  /*background*/
+  "#FFFFFF",
+  "#353B40",
+  "#5E6366",
+  /*text*/
+  "#000000",
+];
 var activeMode = 0;
 
 var isKeyPressed = {
-  'Enter': false
-}
+  Enter: false,
+};
 document.onkeydown = (keyDownEvent) => {
   isKeyPressed[keyDownEvent.key] = true;
   if (isKeyPressed["Enter"]) {
@@ -11,17 +24,22 @@ document.onkeydown = (keyDownEvent) => {
     isKeyPressed[keyDownEvent.key] = false;
     isKeyPressed["Enter"] = false;
   }
-}
+};
 document.onkeyup = (keyUpEvent) => {
   isKeyPressed[keyDownEvent.key] = false;
-}
+};
 
 function addtask() {
   if ($("#newTask").val().trim() != "") {
-    $(".list-group").append(`<li class="list-group-item"><i class="far fa-circle"></i>` + "\xa0\xa0\xa0\xa0" + $("#newTask").val() + `</li>`);
+    $(".list-group").append(
+      `<li class="list-group-item"><i class="far fa-circle"></i>` +
+        "\xa0\xa0\xa0\xa0" +
+        $("#newTask").val() +
+        `</li>`
+    );
     $("#newTask").val("");
 
-    if ($('.important').is(':checked') == 1)
+    if ($(".important").is(":checked") == 1)
       $("li").last().addClass("importantItems");
     if (activeMode == 1) {
       $("li").last().css("color", colors[0]);
@@ -34,7 +52,7 @@ function addtask() {
 
 $("#addTask").click(() => {
   addtask();
-})
+});
 
 /*
 $("#changeColor").click(() => {
@@ -50,52 +68,50 @@ $("#changeColor").click(() => {
 })
 */
 
-$("#darkMode").on('change', function () {
-  if ($('#darkMode').is(':checked')) {
+$("#darkMode").on("change", function () {
+  if ($("#darkMode").is(":checked")) {
     activeMode = 1;
     $("body").css("background-color", colors[1]);
     $("body").css("color", colors[0]);
     $("#newTask").css("background-color", colors[2]);
     $("li").css("color", colors[0]);
     $("li").css("background-color", colors[2]);
-    $("#newTask").addClass('lightgray');
+    $("#newTask").addClass("lightgray");
     $("#newTask").css("color", colors[0]);
-    
-  }
-  else if ($('#darkMode').is(':not(:checked)')) {
+  } else if ($("#darkMode").is(":not(:checked)")) {
     activeMode = 0;
     $("body").css("background-color", colors[0]);
     $("body").css("color", colors[3]);
     $("#newTask").css("background-color", colors[0]);
     $("li").css("background-color", colors[0]);
     $("li").css("color", colors[3]);
-    $("#newTask").removeClass('lightgray');
+    $("#newTask").removeClass("lightgray");
     $("#newTask").css("color", colors[3]);
   }
-})
+});
 
 $("#clearAll").click(() => {
-  $("#list").children().fadeOut("normal", function () {
-    $("#list").children().remove();
-  });
-})
+  $("#list")
+    .children()
+    .fadeOut("normal", function () {
+      $("#list").children().remove();
+    });
+});
 
-
-$(document).on('mouseenter', 'li', function () {
+$(document).on("mouseenter", "li", function () {
   //$(this).css("text-decoration", "line-through");
 
   $(this).children().remove();
   $(this).prepend(`<i class="far fa-times-circle"></i>`);
 });
-$(document).on('mouseleave', 'li', function () {
+$(document).on("mouseleave", "li", function () {
   //$(this).css("text-decoration", "none");
 
   $(this).children().remove();
   $(this).prepend(`<i class="far fa-circle"></i>`);
 });
 
-
-$(document).on('click', 'li', function () {
+$(document).on("click", "li", function () {
   //$(this).css("text-decoration", "line-through");
   $(this).children().remove();
   $(this).prepend(`<i class="far fa-times-circle"></i>`);
