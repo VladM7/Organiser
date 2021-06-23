09b881abf9170dc46e0231c466f31eaccb22f11e
VladM7 <mvlad2005@outlook.com>
2021-06-23 18:43:55 +0300

    code cleaning, reimplemented clear all function

--- script.js
+++ script.js
@@ -81,6 +81,11 @@ function addtask(textData, appendData) {
 }
 
 function clearAll() {
+  currentText = "";
+  fs.writeFile(pathName, currentText, function (err) {
+    if (err) throw err;
+    console.log("Deleted all");
+  });
   $(".list-group")
     .children()
     .fadeOut("normal", function () {
@@ -88,99 +93,75 @@ function clearAll() {
     });
 }
 
-$(document).keyup(function (event) {
-  if (event.which === 13) {
-    addtask($("#newTask").val(), 1);
-    $("#newTask").val("");
-  }
-});
-
-ipcRenderer.on("clear-all", (event) => {
-  clearAll();
-  //console.log("cleared all");
-});
-
-$("#addTask").on("click", () => {
-  let taskText = $("#newTask").val();
-  addtask(taskText, 1);
-});
-
-/*
-$("#changeColor").click(() => {
-  var randomColor1 = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  $("body").css("background-color", randomColor1);
-
-  var randomColor2 = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  $(".list-group-item").css("background-color", randomColor2);
-  $(".importantItems").css("background-color", "#eb5b5b");
-  $("#newTask").css("background-color", randomColor2);
-
-  currentListColor = randomColor2;
-})
-*/
-
-$("#darkMode").on("change", function () {
-  if ($("#darkMode").is(":checked")) {
-    activeMode = 1;
-    $("body").css("background-color", colors[1]);
-    $("body").css("color", colors[0]);
-    $("#newTask").css("background-color", colors[2]);
-    $(".item").css("color", colors[0]);
-    $(".item").css("background-color", colors[2]);
-    $("#newTask").addClass("lightgray");
-    $("#newTask").css("color", colors[0]);
-  } else if ($("#darkMode").is(":not(:checked)")) {
-    activeMode = 0;
-    $("body").css("background-color", colors[0]);
-    $("body").css("color", colors[3]);
-    $("#newTask").css("background-color", colors[0]);
-    $(".item").css("background-color", colors[0]);
-    $(".item").css("color", colors[3]);
-    $("#newTask").removeClass("lightgray");
-    $("#newTask").css("color", colors[3]);
-  }
-});
-
-$("#clearAll").on("click", () => {
-  clearAll();
-});
-
-$(document).on("mouseenter", ".item", function () {
-  //$(this).css("text-decoration", "line-through");
-
-  $(this).children("i").remove();
-  $(this).prepend(`<i class="far fa-times-circle"></i>`);
-});
-$(document).on("mouseleave", ".item", function () {
-  //$(this).css("text-decoration", "none");
-
-  $(this).children("i").remove();
-  $(this).prepend(`<i class="far fa-circle"></i>`);
-});
-
-$(document).on("click", ".item", function () {
-  //$(this).css("text-decoration", "line-through");
-  /* var fileContents = fs.readFileSync(pathName).toString().split("\n");
-  var lineNumber = fileContents.indexOf(
-    $(this).text().replace("\xa0", "").trim()
-  ); */
-  currentText = fs.readFileSync(pathName).toString();
-  //console.log($(this).text().replace("\xa0", "").trim() + "  " + currentText);
-  currentText = currentText.replace(
-    "\n" + $(this).text().replace("\xa0", "").trim() + "\n",
-    ""
-  );
-  fs.writeFileSync(pathName, currentText, function (err) {
-    if (err) return console.log("Error deleting task");
-  });
-
-  $(this).children("i").remove();
-  $(this).prepend(`<i class="far fa-times-circle"></i>`);
-  $(this).fadeOut("slow", function () {
-    $(this).remove();
-  });
-});
-
 $(function () {
   init();
+  $("#addTask").on("click", () => {
+    let taskText = $("#newTask").val();
+    addtask(taskText, 1);
+  });
+  $(document).keyup(function (event) {
+    if (event.which === 13) {
+      addtask($("#newTask").val(), 1);
+      $("#newTask").val("");
+    }
+  });
+  $(document).on("click", ".item", function () {
+    //$(this).css("text-decoration", "line-through");
+    /* var fileContents = fs.readFileSync(pathName).toString().split("\n");
+    var lineNumber = fileContents.indexOf(
+      $(this).text().replace("\xa0", "").trim()
+    ); */
+    currentText = fs.readFileSync(pathName).toString();
+    //console.log($(this).text().replace("\xa0", "").trim() + "  " + currentText);
+    currentText = currentText.replace(
+      "\n" + $(this).text().replace("\xa0", "").trim() + "\n",
+      ""
+    );
+    fs.writeFileSync(pathName, currentText, function (err) {
+      if (err) return console.log("Error deleting task");
+    });
+
+    $(this).children("i").remove();
+    $(this).prepend(`<i class="far fa-times-circle"></i>`);
+    $(this).fadeOut("slow", function () {
+      $(this).remove();
+    });
+  });
+  $(document).on("mouseenter", ".item", function () {
+    //$(this).css("text-decoration", "line-through");
+
+    $(this).children("i").remove();
+    $(this).prepend(`<i class="far fa-times-circle"></i>`);
+  });
+  $(document).on("mouseleave", ".item", function () {
+    //$(this).css("text-decoration", "none");
+
+    $(this).children("i").remove();
+    $(this).prepend(`<i class="far fa-circle"></i>`);
+  });
+  ipcRenderer.on("clear-all", (event) => {
+    clearAll();
+    //console.log("cleared all");
+  });
+  $("#darkMode").on("change", function () {
+    if ($("#darkMode").is(":checked")) {
+      activeMode = 1;
+      $("body").css("background-color", colors[1]);
+      $("body").css("color", colors[0]);
+      $("#newTask").css("background-color", colors[2]);
+      $(".item").css("color", colors[0]);
+      $(".item").css("background-color", colors[2]);
+      $("#newTask").addClass("lightgray");
+      $("#newTask").css("color", colors[0]);
+    } else if ($("#darkMode").is(":not(:checked)")) {
+      activeMode = 0;
+      $("body").css("background-color", colors[0]);
+      $("body").css("color", colors[3]);
+      $("#newTask").css("background-color", colors[0]);
+      $(".item").css("background-color", colors[0]);
+      $(".item").css("color", colors[3]);
+      $("#newTask").removeClass("lightgray");
+      $("#newTask").css("color", colors[3]);
+    }
+  });
 });
